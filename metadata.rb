541dff99fe6b3c07b550b9d5527a12c316e508e1
Daniel Paul Searles <daniel@flowboard.com>
2014-11-21 16:24:44 -0800

    Adds supported platform to the metadata.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'daniel.paul.searles@gmail.com'
 license 'MIT'
 description 'Installs/Configures squaresurf_influxdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
+
+supports 'ubuntu'
