043d89f65a7ecf3011e965e6abd9b263ec3e32b5
Daniel Paul Searles <daniel@flowboard.com>
2014-11-21 16:18:17 -0800

    v0.1.1: Fixes README.md typos for chef supermarket.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'daniel.paul.searles@gmail.com'
 license 'MIT'
 description 'Installs/Configures squaresurf_influxdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
